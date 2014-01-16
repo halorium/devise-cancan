8753e666c5c6b3cd6ba30be5455aa62681fb41fd
Loren Hale <loren.hale@gmail.com>
2014-01-16 15:17:43 -0800

    Initial commit

---
+++ devise-cancan/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseCancan::Application.config.secret_token = 'ef8844bbf375fce2f0c8985952616f8a07b331d0118035308fc1e4a94f2df940fcc1465228454af9a5870f592d0dba25e6e3c5ca80a3ef32092bacf02e8f6410'
